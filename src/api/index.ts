208dd2505c8ed1af0f5d9f39fda0af6884cdb6f1
itsankitbhusal <ankitbhusal95@gmail.com>
2024-05-07 15:30:20 +0545

    apis data fetching

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
 import environments from '../environments';
-import apiRoutes from '../constants/index';
+import { SPELLS } from '../constants/index';
 
 export const fetchSpellsAPI = async () => {
-  const response = await axios.get(
-    `${environments.apiBaseUrl}/${apiRoutes.spells}`
-  );
+  const response = await axios.get(`${environments.apiBaseUrl}/${SPELLS}`);
+  return response.data;
+};
 
+export const fetchSpellDetailAPI = async (spell: string) => {
+  const response = await axios.get(
+    `${environments.apiBaseUrl}/${SPELLS}/${spell}`
+  );
   return response.data;
 };
