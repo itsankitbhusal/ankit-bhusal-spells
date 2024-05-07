0e054c6c99af3dedc877d9729462c76099b42a10
itsankitbhusal <ankitbhusal95@gmail.com>
2024-05-07 15:32:49 +0545

    dynamic homepage to show fav spells

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,19 +1,40 @@
-import { toast } from 'react-toastify';
 import SpellList from '../../components/SpellList';
+import { useSpellContext } from '../../hooks/useSpellContext';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import ToggleView from '../../components/ToggleView';
 
 export default function HomePage() {
-  const handleClick = () => {
-    toast.success('Toast is working', {
-      position: 'top-center',
-    });
+  const { spellList, favSpells } = useSpellContext();
+  const [searchParam] = useSearchParams();
+  const navigate = useNavigate();
+
+  const fav = searchParam.get('fav');
+
+  const handleLinkClick = () => {
+    if (fav) {
+      navigate('/');
+    } else {
+      navigate('?fav=true');
+    }
   };
   return (
-    <div className=' w-10/12 max-w-[1200px]'>
-      {/* <button className=" px-4 py-2 bg-blue-400 rounded" onClick={handleClick}>
-        Click me
-      </button> */}
-
-      <SpellList />
+    <div className=" mx-2 sm:mx-0 sm:w-10/12 max-w-[1200px]">
+      <div>
+        <div className="my-16">
+          <h1 className="font-bold text-3xl text-center">
+            List of all available {fav ? 'Favorite ' : ''}Spells
+          </h1>
+        </div>
+        <div className="w-full text-end">
+          <ToggleView
+            label={fav ? 'View all spells list' : 'View list of favorite'}
+            onClick={handleLinkClick}
+          />
+        </div>
+        <div className="grid place-items-center">
+          <SpellList spellList={fav ? favSpells : spellList} />
+        </div>
+      </div>
     </div>
   );
 }
