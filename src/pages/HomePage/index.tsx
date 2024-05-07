755012e660f56b06f139ecf817d4f7cf31119841
ankitbhusal20 <ankit@bottle.com.np>
2024-05-07 20:38:32 +0545

    render prop for conditional rendering cards

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,10 +1,8 @@
 import SpellList from '../../components/SpellList';
-import { useSpellContext } from '../../hooks/useSpellContext';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import ToggleView from '../../components/ToggleView';
-
+import { ALL, FAV } from '../../constants';
 export default function HomePage() {
-  const { spellList, favSpells } = useSpellContext();
   const [searchParam] = useSearchParams();
   const navigate = useNavigate();
 
@@ -25,14 +23,14 @@ export default function HomePage() {
             List of all available {fav ? 'Favorite ' : ''}Spells
           </h1>
         </div>
-        <div className="w-full text-end">
+        <div className="w-full text-end mb-2">
           <ToggleView
             label={fav ? 'View all spells list' : 'View list of favorite'}
             onClick={handleLinkClick}
           />
         </div>
         <div className="grid place-items-center">
-          <SpellList spellList={fav ? favSpells : spellList} />
+          <SpellList render={fav ? FAV : ALL} />
         </div>
       </div>
     </div>
